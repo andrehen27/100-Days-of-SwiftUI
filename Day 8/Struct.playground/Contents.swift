584a25cde8a6ab5765329686bfafd01e01e775ea
andrehen27 <andre.hendrawan27@yahoo.com>
2021-05-02 01:01:47 +0700

    second commit

--- Day 8/Struct.playground/Contents.swift
+++ Day 8/Struct.playground/Contents.swift
@@ -8,4 +8,18 @@ struct Vehicle{
 var honda = Vehicle(name: "Mazda")
 print(honda.name)
 
-
+struct Championship{
+    var name: String
+    var isBigFour: Bool
+    
+    var isChampionshipStatus: String{
+        if isBigFour{
+            return "\(name) is going to UCL"
+        }else{
+            return "\(name) isn't going to UCL"
+        }
+    }
+}
+//isChampionship is Computed Property
+let everton = Championship(name: "Everton", isBigFour: false)
+print(everton.isChampionshipStatus)
